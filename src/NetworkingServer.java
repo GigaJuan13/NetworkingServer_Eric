20de347b4ab19d0304a738c9cab3ec6bd48d984e
Eric Jonsson <eric.jonssonn@elev.ga.ntig.se>
2024-05-23 11:31:32 +0200

    skrivit lite mer kod

--- src/NetworkingServer.java
+++ src/NetworkingServer.java
@@ -1,4 +1,5 @@
 /* SERVER - may be enhanced to work for multiple clients */
+import java.io.IOException;
 import java.net. *;
 
 public class NetworkingServer {
@@ -13,7 +14,20 @@ public class NetworkingServer {
         }
 
         // Creat server side socket
+        try {
+            server = new ServerSocket(portnumber);
+        }
+        catch (IOException ie) {
+            System.out.println("Can't open socket." + ie);
+            System.exit(1);
+        }
+        System.out.println("ServerSocket is created " + server);
 
-
+        // Wait for the data from client and reply
+        while (true){
+            try {
+                // Listens for a connection to be made to
+            }
+        }
     }
 }
