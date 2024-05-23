8732efa81d40bf73713f29abb535810fde97a51c
Eric Jonsson <eric.jonssonn@elev.ga.ntig.se>
2024-05-23 11:01:06 +0200

    har precis börjat med koden för servern

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
+/* SERVER - may be enhanced to work for multiple clients */
+import java.net. *;
+import java.io. *;
+
+public class NetworkingServer_Eric {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    ServerSocket server = null;
+    Socket client;
+
+    // Default port number we are going to use
+        int portnumber = 1234;
+        if (args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+        // Creat server side socket
+
     }
 }
